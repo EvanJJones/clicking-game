26342ba84813a65f0c4360263fa615ca550edb7c
Evan Jones <evanjones@fastmail.net>
2019-11-03 20:22:19 -0500

    Updates

---
+++ precache-manifest.72c36ec301dca8eaf7cdf37f7c897f65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05cc118a2e407cc59da425fcb5e6cd3",
+    "url": "/clicking-game/index.html"
+  },
+  {
+    "revision": "3fad21896a763e0a19f7",
+    "url": "/clicking-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "40a885ad161d7a52f6d7",
+    "url": "/clicking-game/static/js/2.5daff98e.chunk.js"
+  },
+  {
+    "revision": "3fad21896a763e0a19f7",
+    "url": "/clicking-game/static/js/main.cbf99f49.chunk.js"
+  },
+  {
+    "revision": "9d28b0299127799f50cc",
+    "url": "/clicking-game/static/js/runtime-main.d0e5c838.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/clicking-game/precache-manifest.72c36ec301dca8eaf7cdf37f7c897f65.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicking-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
