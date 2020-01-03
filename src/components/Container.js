18dd54873abb6e578ef381db5e03357586232c54
Evan Jones <evanjones@fastmail.net>
2020-01-03 11:37:26 -0500

    added comments

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,16 @@
 import React from "react";
 
+// container that holds everything else
+
 const styles = {
   width: "100%",
   height: "80%",
   display: "flex",
   justifyContent: "space-evenly",
   flexWrap: "wrap"
-
-}
+};
 function Container(props) {
-  return (
-    <div style={styles}>
-      {props.children}
-    </div>
-  );
+  return <div style={styles}>{props.children}</div>;
 }
 
 export default Container;
