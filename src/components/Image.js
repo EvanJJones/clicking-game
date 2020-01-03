5015d77dd540f42f2cd8fb5c4c49851a452a03e7
Evan Jones <evanjones@fastmail.net>
2020-01-03 11:35:50 -0500

    added comments

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,32 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Image extends Component {
   state = {
+    // whether the card is flipped or not
     flipped: false,
+    // whether the card is matched or not
     matched: false,
     lastClicked: 0
-  }
+  };
 
-  clickImage = (id, match) => {;
+  clickImage = (id, match) => {
     if (this.state.lastClicked === 0) {
-      this.setState({lastClicked: id});
+      this.setState({ lastClicked: id });
     } else if (this.state.lastClicked === match) {
-      this.setState({lastClicked: 0, matched: true});
+      this.setState({ lastClicked: 0, matched: true });
     } else {
-      this.setState({lastClicked: 0});
+      this.setState({ lastClicked: 0 });
     }
-
   };
 
   flipCard = () => {
     console.log(this.props.id);
-    this.setState({flipped: true})
-  }
+    this.setState({ flipped: true });
+  };
   render() {
     return (
-
-    <img src={this.props.flipped ? this.props.src : this.props.back} alt={this.props.alt} width="16.5%" className="img-thumbnail" onClick={() => this.props.clickImage(this.props.id, this.props.match)}></img>
-    )
+      <img
+        src={this.props.flipped ? this.props.src : this.props.back}
+        alt={this.props.alt}
+        width="16.5%"
+        className="img-thumbnail"
+        onClick={() => this.props.clickImage(this.props.id, this.props.match)}
+      ></img>
+    );
   }
 }
 
